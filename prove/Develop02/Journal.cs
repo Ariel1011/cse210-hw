6d2f0cc9ada5ae20cd4a2823fca8c9b04110ee44
Ariel <ariel@example>
2023-09-21 22:19:10 +0800

    Created a Design

---
+++ prove/Develop02/Journal.cs
@@ -0,0 +1,25 @@
+public class Journal
+{
+    public List<Entry>_entries;
+
+    public void AddEntry(Entry newEntry)
+    {
+
+    }
+
+    public void DisplayAll()
+    {
+
+    }
+
+    public void SaveTofile(string file)
+    {
+
+    }
+    public void LoadFromFile(string file)
+
+    {
+
+    }
+
+}
