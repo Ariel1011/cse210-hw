f074c3d4f681a56131e4ab59c9302cc1e0b97b93
Ariel <ariel@example>
2023-09-23 21:23:04 +0800

    Finished program.cs

--- prove/Develop02/Program.cs
+++ prove/Develop02/Program.cs
@@ -4,6 +4,53 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Develop02 World!");
+        PromptGenerator promptGenerator = new PromptGenerator();
+        Journal journal = new Journal();
+
+        while (true)
+        {
+            Console.WriteLine("Welcome to My Journal:");
+            Console.WriteLine("1. Write");
+            Console.WriteLine("2. Display");
+            Console.WriteLine("3. Save");
+            Console.WriteLine("4. Load");
+            Console.WriteLine("5. Quit");
+            Console.Write("What would you like to do? ");
+
+            string choice = Console.ReadLine();
+
+            switch (choice)
+            {
+                case "1":
+                    string prompt = promptGenerator.GetRandomPrompt();
+                    Console.WriteLine(prompt);
+                    string date = DateTime.Now.ToShortDateString();
+
+                    string entryText = Console.ReadLine();
+                    Entry entry = new Entry(date, prompt, entryText);
+                    journal.AddEntry(entry);
+                    break;
+                case "2":
+                    journal.DisplayAll();
+                    break;
+                case "3":
+                    Console.Write("Enter a filename to save the journal: ");
+                    string saveFilename = Console.ReadLine();
+                    journal.SaveToFile(saveFilename);
+                    break;
+                case "4":
+                    Console.Write("Enter a filename to load the journal: ");
+                    string loadFilename = Console.ReadLine();
+                    journal.LoadFromFile(loadFilename);
+                    break;
+                case "5":
+                    Environment.Exit(0);
+                    break;
+                default:
+                    Console.WriteLine("Invalid choice. Please try again.");
+                    break;
+            }
+        }
     }
 }
+
