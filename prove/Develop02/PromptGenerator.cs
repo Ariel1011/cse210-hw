1670d60d9cedb31be99602ceedced7b8ffd045ec
Ariel <ariel@example>
2023-09-23 21:24:17 +0800

    Finished PromptGenerator

--- prove/Develop02/PromptGenerator.cs
+++ prove/Develop02/PromptGenerator.cs
@@ -1,9 +1,26 @@
+using System;
+using System.Collections.Generic;
+
 public class PromptGenerator
 {
-    public List<string>_promts;
-    
+    public List<string> _prompts;
+
+    public PromptGenerator()
+    {
+        _prompts = new List<string>
+        {
+            "What is my overall intention for this day?",
+            "What was the best part of your day?",
+            "How did you see the hand of the Lord in your life today?",
+            "What do I need to be mindful of today?",
+            "What did you learn today?"
+        };
+    }
+
     public string GetRandomPrompt()
     {
-        return "";
+        Random random = new Random();
+        int index = random.Next(_prompts.Count);
+        return _prompts[index];
     }
 }
